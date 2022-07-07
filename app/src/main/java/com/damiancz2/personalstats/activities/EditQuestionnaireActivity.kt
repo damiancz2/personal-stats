508f16f608c1acc65a435f3c0933a0e04e657304
damiancz2 <dczarnecki2@gmail.com>
2022-07-07 20:01:21 +0200

    Make questionnaire name editable after creation

--- app/src/main/java/com/damiancz2/personalstats/activities/EditQuestionnaireActivity.kt
+++ app/src/main/java/com/damiancz2/personalstats/activities/EditQuestionnaireActivity.kt
@@ -2,12 +2,18 @@ package com.damiancz2.personalstats.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.damiancz2.personalstats.QUESTIONNAIRE_ID
 import com.damiancz2.personalstats.QUESTIONNAIRE_NAME
 import com.damiancz2.personalstats.R
+import com.damiancz2.personalstats.getQuestionnaires
+import com.damiancz2.personalstats.model.Questionnaire
+import com.damiancz2.personalstats.saveQuestionnaires
 
 class EditQuestionnaireActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +28,10 @@ class EditQuestionnaireActivity : AppCompatActivity() {
             questionnaireName = bundle.getString(QUESTIONNAIRE_NAME)
 
             val textView: TextView = findViewById(R.id.QuestionnaireNameInputTextBox)
+            val editText: EditText = findViewById(R.id.QuestionnaireNameInputEditText)
+
             textView.text = questionnaireName
+            editText.setText(questionnaireName, TextView.BufferType.EDITABLE)
 
             val addQuestionButton: Button = findViewById(R.id.AddQuestionButton)
             addQuestionButton.setOnClickListener {
@@ -30,6 +39,30 @@ class EditQuestionnaireActivity : AppCompatActivity() {
                 intent.putExtra(QUESTIONNAIRE_NAME, questionnaireName)
                 intent.putExtra(QUESTIONNAIRE_ID, questionnaireId)
                 startActivity(intent)
+            }
+
+            val editQuestionButton: ImageView = findViewById(R.id.btnEditQuestionnaireName)
+            editQuestionButton.setOnClickListener{
+                val tick: ImageView = findViewById(R.id.btnApproveQuestionnaireName)
+                editText.visibility = View.VISIBLE
+                tick.visibility = View.VISIBLE
+                editQuestionButton.visibility = View.GONE
+                textView.visibility = View.GONE
+                tick.setOnClickListener{
+                    val questionnaires: ArrayList<Questionnaire> = getQuestionnaires(this)
+                    val questionnaire = questionnaires.filter{it.id == questionnaireId}[0]
+                    val index = questionnaires.indexOf(questionnaire)
+                    val newQuestionnaire = Questionnaire(questionnaireId, editText.text.toString())
+                    questionnaires.remove(questionnaire)
+                    questionnaires.add(index, newQuestionnaire)
+                    saveQuestionnaires(this, questionnaires)
+
+                    textView.text = editText.text.toString()
+                    editText.visibility = View.GONE
+                    tick.visibility = View.GONE
+                    editQuestionButton.visibility = View.VISIBLE
+                    textView.visibility = View.VISIBLE
+                }
             }
         }
 
